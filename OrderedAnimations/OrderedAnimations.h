f0208c39ccb9202d696c4d4918b06477d5628b8e
Brandon S. Kane <me@brandonskane.com>
2017-02-20 22:12:59 -0500

    Initial Commit

---
+++ OrderedAnimations/OrderedAnimations.h
@@ -0,0 +1,19 @@
+//
+//  OrderedAnimations.h
+//  OrderedAnimations
+//
+//  Created by Brandon S. Kane on 2/20/17.
+//  Copyright © 2017 Brandon S. Kane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrderedAnimations.
+FOUNDATION_EXPORT double OrderedAnimationsVersionNumber;
+
+//! Project version string for OrderedAnimations.
+FOUNDATION_EXPORT const unsigned char OrderedAnimationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrderedAnimations/PublicHeader.h>
+
+
